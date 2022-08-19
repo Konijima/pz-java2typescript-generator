45c9007880684f0f5ad1e714e784e7b521c4fb8a
Konijima <mathieugermain1991@gmail.com>
2022-08-18 23:28:24 -0400

    Clean up index.ts

--- app/index.ts
+++ app/index.ts
@@ -3,21 +3,28 @@ import { generateTS } from "./modules/TSGenerator";
 import { parseArgv } from "./modules/Utilities";
 
 const ARGS = parseArgv();
-const SRC_PATH = ARGS.src || "src";
-const JSON_PATH = "json";
-const OUT_PATH = ARGS.out || "out";
+const SRC_PATH = ARGS.src || "src/";
+const JSON_PATH = "json/";
+const OUT_PATH = ARGS.out || "out/";
+const SIZE = ARGS.size || 100;
 
 (async function() {
 
-    console.log("Java Generator initializing...");
-    console.log("Args: ", ARGS);
+    console.log("==========================================");
+    console.log("PZ Java 2 Typescript Generator by Konijima");
+    console.log("==========================================");
+    console.log("SRC path  :", SRC_PATH);
+    console.log("JSON path :", JSON_PATH);
+    console.log("OUT path  :", OUT_PATH);
+    console.log("Size      :", SIZE);
+    console.log("==========================================");
 
     const START_TIME = Date.now();
 
     if (ARGS.json) await generateJSON({
         inputPath: SRC_PATH, 
         outputPath: JSON_PATH,
-        size: ARGS.size || 100
+        size: SIZE
     });
 
     if (ARGS.ts) await generateTS(JSON_PATH, OUT_PATH);
